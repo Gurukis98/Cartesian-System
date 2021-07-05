cbfcd0739d5c6465dd1cc884ae5e5369dbad7ecf
Gurukis98 <kishoreguru01@gmail.com>
2021-07-05 20:48:43 +0530

    Guru

---
+++ Cartesian System UC-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cartesian_System_UC_1
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            int x1 = 4;
+            int x2 = 6;
+            int y1 = 8;
+            int y2 = 10;
+            Console.WriteLine("Point One: " + (x1,y1));
+            Console.WriteLine("Point Two: " + (x2, y2));
+            Console.WriteLine("Length of a Line: " + Math.Sqrt((x2-x1)^2 + (y2-y1)^2));
+        }
+    }
+}
